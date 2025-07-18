b53c7c86e1cd0f287327ca586d11dfb76a0c2014
CoulibalyT-T <chansaki87@gmail.com>
2025-07-18 10:46:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get("/spin", async (req, res) => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            content: `🎯 **${req.session.user.username}** spun and won **${prize}**!`
+            content: `🎯 **${req.session.user.username}** shuffle and won **${prize}**!`
         })
     });
 
